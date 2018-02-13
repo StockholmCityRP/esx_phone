6a28fb58b3bc8632773f6884b49e2d11c8d143b9
Kasper <35449118+KasperE1@users.noreply.github.com>
2018-02-13 09:38:15 +0100

    Create sv.lua

---
+++ locales/sv.lua
@@ -0,0 +1,11 @@
+Locales['sv'] = {
+  ['new_message'] = '~b~Nytt meddelande:~s~ %s',
+  ['press_take_call'] = ' - Tryck ~INPUT_CONTEXT~ för att svara',
+  ['taken_call'] = ' har svarat',
+  ['gps_position'] = 'destenation utmarkerad på kartan',
+  ['message_sent'] = 'meddelande skickat',
+  ['cannot_add_self'] = 'Du kan inte lägga till dig själv!',
+  ['number_in_contacts'] = 'Det här nummret finns redan bland dina kontakter!',
+  ['contact_added'] = 'Kontakt tillagd!',
+  ['number_not_assigned'] = 'Det här nummret finns inte...',
+}
